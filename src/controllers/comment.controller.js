705fdc1f2d52456c1d07736dd61ce060c40df4dc
Ammari045 <Ammari045@users.noreply.github.com>
2024-01-18 08:34:19 +0500

    comment controller finshed

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import { Comment } from "../models/comment.model.js"
 import ApiError from "../utils/ApiError.js"
 import ApiResponse from "../utils/ApiResponse.js"
@@ -7,7 +7,70 @@ import asyncHandler from "../utils/asyncHandler.js"
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params
-    const { page = 1, limit = 10 } = req.query
+    let { page = 1, limit = 10 } = req.query
+
+    page = isNaN(page) ? 1 : Number(page)
+    limit = isNaN(limit) ? 10 : Number(limit)
+
+    if (page <= 0) {
+        page = 1
+    }
+    if (limit <= 0) {
+        limit = 10
+    }
+
+    if (!videoId.trim() || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId")
+    }
+
+
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "commentedBy",
+                pipeline: [
+                    {
+                        $project: {
+                            userName: 1,
+                            fullName: 1,
+                            avatar: 1,
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                commentedBy: {
+                    $first: "$commentedBy"
+                }
+            }
+        },
+        {
+            $skip: (page - 1) * limit
+        },
+        {
+            $limit: limit
+        }
+    ])
+
+    if (comments.length === 0) {
+        throw new ApiError(404, "No comments found");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, comments, "Comments fetched successfully")
+        )
 
 })
 
@@ -22,7 +85,7 @@ const addComment = asyncHandler(async (req, res) => {
 
     const comment = await Comment.create({
         content,
-        videoId
+        video: videoId
     })
 
     if (!comment) {
